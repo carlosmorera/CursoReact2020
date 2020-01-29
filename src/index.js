1de103de72db20c3655ea5e4a5874664524c1448
carlosmorera <carlosmorera@seven4n.com>
2020-01-29 14:33:41 -0500

    Se crea un objeto y se hace el llamado del mismo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDom from 'react-dom'
 
-const App = <h1>Hello React</h1>
+const name = 'Carlos Morera'
+//Build object
+const user ={
+    name : 'Carlos Morera',
+    age: 26,
+    country: 'Colombia'
+}
+
+function getInfo(user) {
+    return `Hi my name is ${user.name} and Im from ${user.country} `
+}
+
+const App = <h1>{getInfo(user)}</h1>
 
 ReactDom.render(App,document.getElementById('root'))
