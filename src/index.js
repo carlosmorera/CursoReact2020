db00c2b6fa4eff109511c97d3bc625ecf0fdba59
carlosmorera <carlosmorera@seven4n.com>
2020-01-29 15:57:22 -0500

    Se cambia componente funcional por una clase

--- src/index.js
+++ src/index.js
@@ -1,24 +1,40 @@
 import React from "react";
 import ReactDom from 'react-dom'
 
-//Define funtional Component
-const FruitTarjet = (props) => {
-    console.log(props)
- return   <div>
-        <h3>{props.name}</h3>
-        <hr/>
-        <p>$ {props.price}</p>
-    </div>
+class FruitTarjet extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cantidad: 0
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h3>{this.props.name}</h3>
+                <hr/>
+                <div>cantidad: {this.state.cantidad}</div>
+                <button onClick={() => {
+                this.setState({cantidad: this.state.cantidad + 1})
+                }}
+                >
+                    Agregar
+                </button>
+                <p>$ {this.props.price}</p>
+            </div>
+        );
+    }
+
 }
 
-
-
-const App = () =>(
+const App = () => (
     <div>
-        <FruitTarjet name= {'Apple'} price={2.00}/>
-        <FruitTarjet name= 'Orange' price={3.00}/>
-        <FruitTarjet name= 'Kiwi'/>
+        <FruitTarjet name={'Apple'} price={2.00}/>
+        <FruitTarjet name='Orange' price={3.00}/>
+        <FruitTarjet name='Kiwi'/>
     </div>
 )
 
-ReactDom.render(<App/>,document.getElementById('root'))
+ReactDom.render(<App/>, document.getElementById('root'))
