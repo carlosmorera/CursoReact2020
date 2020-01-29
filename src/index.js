61f906b7dd37af837246e8a30f89a158cbc7c1f9
carlosmorera <carlosmorera@seven4n.com>
2020-01-29 14:55:03 -0500

    Se le envian prorps al componente funcional

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import React from "react";
 import ReactDom from 'react-dom'
 
 //Define funtional Component
-const FruitTarjet = () => (
-    <div>
-        <hh3>Title</hh3>
-    <hr/>
-    <p>Description...</p>
+const FruitTarjet = (props) => {
+    console.log(props)
+ return   <div>
+        <h3>{props.name}</h3>
+        <hr/>
+        <p>$ {props.price}</p>
     </div>
-)
+}
+
+
 
 const App = () =>(
     <div>
-        <FruitTarjet/>
-        <FruitTarjet></FruitTarjet>
-
+        <FruitTarjet name= {'Apple'} price={2.00}/>
+        <FruitTarjet name= 'Orange' price={3.00}/>
+        <FruitTarjet name= 'Kiwi'/>
     </div>
 )
 
