07be1930981578e9d15007c5f209ebbda6f7c938
carlosmorera <carlosmorera@seven4n.com>
2020-01-30 16:31:49 -0500

    Se crean estilos

--- src/index.js
+++ src/index.js
@@ -2,52 +2,50 @@ import React from "react";
 import ReactDOM from 'react-dom'
 
 class FruitTarjet extends React.Component {
-    constructor() {
-        super();
 
-        const METHODS = [
-            'agregar',
-            'quitar',
-            'limpiar'
-        ];
-
-        METHODS.forEach((method) => {
-            this[method] = this[method].bind(this)
-        })
-
-        this.state = {
-            cantidad: 0
-        }
+    state = {
+        cantidad: 0
     }
 
-    agregar() {
-        this.setState({
-            cantidad: this.state.cantidad + 1
-        })
-    }
+    agregar = () => this.setState({
+        cantidad: this.state.cantidad + 1
+    })
 
-    quitar() {
-        this.setState({
-            cantidad: this.state.cantidad - 1
-        })
-    }
 
-    limpiar() {
-        this.setState({
-            cantidad: 0
-        })
-    }
+    quitar = () => this.setState({
+        cantidad: this.state.cantidad - 1
+    })
+
+
+    limpiar = () => this.setState({
+        cantidad: 0
+    })
+
 
     render() {
+        const hasItems = this.state.cantidad > 0
+        const styles ={
+            border: '1px solid black',
+            marginBottom: '1em',
+            borderRadius: '0.5em',
+            padding: '1em',
+            background: hasItems ? 'linear-gradient(45deg, black, #4a02f7)' : '#FFF',
+            color: hasItems ? '#FFF' : '#000',
+            transition: 'all 400ms ease-out'
+
+        }
         return (
-            <div>
+            <div style={styles}>
                 <h3>{this.props.name}</h3>
-                <hr/>
                 <div>cantidad: {this.state.cantidad}</div>
                 <button onClick={this.agregar}> +</button>
                 <button onClick={this.quitar}> -</button>
                 <button onClick={this.limpiar}> Limpiar</button>
+                <hr/>
                 <p>$ {this.props.price}</p>
+                <p>
+                    Total: $ {this.props.price * this.state.cantidad}
+                </p>
             </div>
         );
     }
