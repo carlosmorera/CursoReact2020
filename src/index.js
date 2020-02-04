cdfa6fbe1fb3c3eb422f0b9c504724e8deb399ad
carlosmorera <carlosmorera@seven4n.com>
2020-02-04 16:32:54 -0500

    se crea un componente el cual se muta el estado

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from "react";
 import ReactDom from 'react-dom'
+import App from "./App";
 
-const name = 'Carlos Morera'
-//Build object
-const user ={
-    name : 'Carlos Morera',
-    age: 26,
-    country: 'Colombia'
-}
 
-function getInfo(user) {
-    return `Hi my name is ${user.name} and Im from ${user.country} `
-}
-
-const App = <h1>{getInfo(user)}</h1>
-
-ReactDom.render(App,document.getElementById('root'))
+ReactDom.render(<App/>,document.getElementById('root'))
