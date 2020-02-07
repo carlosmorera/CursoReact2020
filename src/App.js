80be6ad46e113f771213dd16462710999011e057
carlosmorera <carlosmorera@seven4n.com>
2020-02-07 10:35:57 -0500

    Ejercicio donde se ahcer un portal segundario

--- src/App.js
+++ src/App.js
@@ -1,39 +1,67 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from "react";
+import ReactDOM from 'react-dom'
 
-const Computacion = () => (
-    <Fragment>
-        <li>Pantalla</li>
-        <li>Teclado</li>
-        <li>Mouse</li>
-    </Fragment>
-)
+class PortalModal extends Component {
+    render() {
+        if (this.props.visible) {
+            return null
+        }
 
-
-const Ropa = () => (
-    <Fragment>
-        <li>Playeras</li>
-        <li>Jenas</li>
-        <li>Short</li>
-    </Fragment>
-)
-//En este caso no se usa la etiqueta Fragment pero es lo mismo, lo unico es que no se pueden enviar parametros
-const Auto = () => (
-    <>
-        <li> parte 1</li>
-        <li> parte 2</li>
-    </>
-)
-
+        const styles = {
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            background: 'linear-gradient(to top right, #667eea, #764ba2)',
+            opacity: '095',
+            color: '#FFF'
+        }
+        return ReactDOM.createPortal((
+            <div style={styles}>
+                {this.props.children}
+            </div>
+        ), document.getElementById('modal-root'))
+    }
+}
 
 class App extends Component {
+    state = {
+        visible: false,
+        num: 0
+    }
 
+    componentDidMount() {
+        setInterval(() => {
+            this.setState(state => ({
+                num: state.num + 1
+            }))
+        }, 1000)
+    }
+
+    mostrar = () => {
+        this.setState({visible: false})
+    }
+    cerrar = () => {
+        this.setState({visible: true})
+    }
 
     render() {
         return (
             <div>
-                <Computacion/>
-                <Ropa/>
-                <Auto/>
+                <button
+                    onClick={this.mostrar}>
+                    Mostrar
+                </button>
+                <PortalModal visible={this.state.visible}>
+                    <button
+                        onClick={this.cerrar}>
+                        Cerrar
+                    </button>
+                    <h1>
+                        Hola desde un portal Modal {this.state.num}
+                    </h1>
+                </PortalModal>
             </div>
         )
     }
