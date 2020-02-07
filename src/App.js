683d32985e0bfdc44145636c094341c1896147ca
carlosmorera <carlosmorera@seven4n.com>
2020-02-07 14:58:56 -0500

    Se definnen los defaultProps

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, {Component} from "react";
 import PropTypes from 'prop-types'
 
 //Para instalar propTypes --> npm i prop-types
-/* En una furncion arrow se definen fera de la funcion
+/* En una furncion arrow se definen fuera de la funcion las propTypes
 const Profile = () => ()
-Profile.propTypes = {}*/
+Profile.propTypes = {}
 
+En una furncion arrow se definen fuera de la funcion las defaultProps
+Profile.defaultProps ={}
+*/
+const noop = () => {}
 class Profile extends Component {
 
     //De esta forma se definen ls propTypes dentro de la clase
     static propTypes = {
-        name: PropTypes.string,
+        name: PropTypes.string.isRequired,
         bio: PropTypes.string,
         email: PropTypes.string,
         age: PropTypes.number
+    }
+    static defaultProps ={
+        age: 26,
+        onHellow: noop
+    }
+
+    saluda =() =>{
+        this.props.onHellow()
     }
 
     render() {
@@ -28,10 +40,17 @@ class Profile extends Component {
                     {email}
                 </a>
                 <p>{age}</p>
+                <button onClick={this.saluda}>
+                    Saludar
+                </button>
             </div>
         )
     }
 }
+/* De esta forma se pueden definir las default propTypes fuera de la clase
+
+Profile.defaultProps ={
+}*/
 
 /* De esta forma se pueden definir las propTypes fuera de la clase
 Profile.propTypes = {}*/
@@ -44,7 +63,6 @@ class App extends Component {
                     name='Carlos'
                     bio=' Soy desarrollador'
                     email='mail@mail.com'
-                    age={46}
                 />
             </div>
         )
