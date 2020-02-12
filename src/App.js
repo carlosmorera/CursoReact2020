05d09a2794fbb5cdf573d050c0b802b7194b0d96
carlosmorera <carlosmorera@seven4n.com>
2020-02-12 14:37:34 -0500

    ejerciciod e como se maneja un input no controlado dentro de un formulario

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import React, {Component} from "react";
 
-const FancyInput = React.forwardRef((props, ref) => (
+//Con los input no controlados no podermos controlar los cambios de la informacion
 
-    <div>
-        <input
-            type="text"
-            ref={ref}
-        />
-    </div>
-))
+class InputNoControlado extends Component {
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const nombre = event.target[0].value
+        const email = event.target[1].value
+        this.props.onSend({nombre, email})
+    }
 
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder='Nombre'
+                />
+                <input
+                    type="text"
+                    placeholder='Email'
+                />
+                <button>
+                    Enviar
+                </button>
+            </form>
+        )
+    }
+
+}
 
 class App extends Component {
-    entrada = React.createRef()
-    componentDidMount() {
-        console.log(this.entrada)
+    send = (data) => {
+        console.log(data)
     }
 
     render() {
         return (
             <div>
-                <h1>Renvio de refs</h1>
-                <FancyInput
-                    ref={this.entrada}/>
+                <h1>Inputs no controlados Formularios</h1>
+                <InputNoControlado
+                onSend ={this.send}
+                />
             </div>
         )
     }
