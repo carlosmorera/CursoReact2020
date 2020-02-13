18ca120ff16c1c1965fcdaf873cbc979f92f1fa2
carlosmorera <carlosmorera@seven4n.com>
2020-02-13 14:22:30 -0500

    Ejercico de seleccion de lista

--- src/App.js
+++ src/App.js
@@ -1,57 +1,32 @@
 import React, {Component} from "react";
 
-//Con los input no controlados no podermos controlar los cambios de la informacion
-
-class InputNoControlado extends Component {
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const nombre = event.target[0].value
-        const email = event.target[1].value
-        this.props.onSend({nombre, email})
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-
-                <p>
-                    <label htmlFor='name'>Nombre: </label>
-                    <input
-                        type="text"
-                        placeholder='Nombre'
-                        id='name'
-                    />
-                </p>
-                <p>
-                    <label htmlFor='email'>Email: </label>
-                    <input
-                        type="text"
-                        placeholder='Email'
-                        id='email'
-                    />
-                </p>
-
-                <button>
-                    Enviar
-                </button>
-            </form>
-        )
-    }
-
-}
-
 class App extends Component {
-    send = (data) => {
-        console.log(data)
+    state = {
+        tech : 'Vue'
     }
-
+    handleChange = (event) =>{
+        this.setState({
+            tech: event.target.value
+        })
+    }
     render() {
         return (
             <div>
-                <h1>Inputs no controlados Formularios</h1>
-                <InputNoControlado
-                    onSend={this.send}
-                />
+                <h1>
+                    Etiqueta Select
+                   <p>
+                       {this.state.tech}
+                   </p>
+                </h1>
+
+                <form>
+                    <select  value={this.state.tech} onChange={this.handleChange}>
+                        <option value="Angular">Angular</option>
+                        <option value="React">React</option>
+                        <option value="Vue">Vue</option>
+                        <option value="Vanilla">Vanilla</option>
+                    </select>
+                </form>
             </div>
         )
     }
