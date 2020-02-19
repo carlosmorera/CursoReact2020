a231242a45dc6e0423a28a72427ef42759efbca1
carlosmorera <carlosmorera@seven4n.com>
2020-02-19 16:47:19 -0500

    ejercicio donde se explica la utilidad del componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,45 +1,71 @@
 import React, {Component} from "react";
+import {render} from "react-dom";
 
-class Contador extends Component {
-    // Esta es la forma adecauada de usar el constructor del componente
-    /*    constructor(props) {
-        super(props);
-        this.agregar = this.agregar.bind(this)
-        this.
-        }
-    }*/
+class Http extends Component {
     state = {
-        num: this.props.num
+        photos: []
     }
-    agregar = () => {
-        this.setState(state => ({
-            num: state.num + 1
-        }))
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+            .then(res => res.json())
+            .then(photos => this.setState({photos}))
     }
+
     render() {
+        const {photos} = this.state
         return (
             <div>
-                <button
-                    onClick={this.agregar}
-                >
-                    click({this.state.num})
-                </button>
+                {photos.map(photo => (
+                    <img
+                        key={photo.id}
+                        src={photo.thumbnailUrl}
+                        alt={photo.title}
+                    />
+
+                ))}
             </div>
         )
     }
 }
 
-class App extends Component {
+class Events extends Component {
+
+    state = {
+        width: window.innerWidth
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.handlerResize)
+    }
+
+    handlerResize = () => {
+        this.setState({
+            width: window.innerWidth
+        })
+    }
 
     render() {
         return (
             <div>
-                <h1>Metodo constructor</h1>
-                <Contador
-                    num={100}
-                /> <Contador
-                num={10}
-            />
+                <h2>Events {this.state.width}</h2>
+            </div>
+        )
+    }
+}
+
+
+class App extends Component {
+    componentDidMount() {
+        //Es usado para hacer solicitudes HTTP
+        //Tambien para agregar Event Listeners
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>componentDidMount</h1>
+                <Events/>
             </div>
         )
     }
